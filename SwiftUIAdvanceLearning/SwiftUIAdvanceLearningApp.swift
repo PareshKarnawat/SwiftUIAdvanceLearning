1f250ed16f29c3148d9244865799bce88192a293
Paresh Karnawat <paresh.karnawat@gmail.com>
2025-10-03 22:59:34 +0530

    View Design changed

--- SwiftUIAdvanceLearning/SwiftUIAdvanceLearningApp.swift
+++ SwiftUIAdvanceLearning/SwiftUIAdvanceLearningApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct SwiftUIAdvanceLearningApp: App {
+    init() {
+            let appearance = UINavigationBarAppearance()
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
     var body: some Scene {
         WindowGroup {
-            SwiftUIViewModifier()
+            NavigationStack {
+                SwiftUIViewModifier()
+            }
         }
     }
 }
--- SwiftUIAdvanceLearning/ViewModifiers/SwiftUIViewModifier.swift
+++ SwiftUIAdvanceLearning/ViewModifiers/SwiftUIViewModifier.swift
@@ -9,32 +9,47 @@ import SwiftUI
 
 struct SwiftUIViewModifier: View {
     var body: some View {
-        VStack(spacing: 20){
-              Button("Hello, Geen!") {
-                // Add action here
-            }
-            .font(.headline)
-            .withButtonFormatting(backgroundColor: .green)
+        ZStack {
+            // Background: gray with white gradient overlay
+            Color.black
+                .ignoresSafeArea()
+                .overlay(
+                    LinearGradient(
+                        gradient: Gradient(colors: [.white.opacity(0.4), .clear]),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                    .ignoresSafeArea()
+                )
             
-            Button("Hello, Orange!") {
-                // Add action here
+            VStack(spacing: 20){
+                Button("Hello, Geen!") {
+                    // Add action here
+                }
+                .font(.headline)
+                .withButtonFormatting(backgroundColor: .green)
+                
+                Button("Hello, Orange!") {
+                    // Add action here
+                }
+                .font(.subheadline)
+                .withButtonFormatting(backgroundColor: .orange)
+                
+                Button("Hello, Red!") {
+                    // Add action here
+                }
+                .font(.title)
+                .withButtonFormatting(backgroundColor: .red)
+                
+                Button("Hello Blue!!") {
+                    // Add action here
+                }
+                .font(.title3)
+                .withButtonFormatting()
             }
-            .font(.subheadline)
-            .withButtonFormatting(backgroundColor: .orange)
-            
-            Button("Hello, Red!") {
-                // Add action here
-            }
-            .font(.title)
-            .withButtonFormatting(backgroundColor: .red)
-            
-            Button("Hello Blue!!") {
-                // Add action here
-            }
-            .font(.title3)
-            .withButtonFormatting()
+            .padding()
         }
-        .padding()
+        .navigationTitle("ViewModifier Demo")
     }
 }
 
